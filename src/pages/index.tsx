5ac0a84e8a34b7574aff3477125e0f9c06132d85
yunish-i <yunyiii1376@outlook.com>
2025-01-27 16:51:01 -0500

    message

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@
 // import Image from 'next/image'
 // import { Inter } from 'next/font/google'
 // import styles from '@/styles/Home.module.css'
-import Gallery from "./qcomps/gallery_props";
+import Gallery from "./qcomps/recipes";
 
 export default function Home() {
   return <Gallery />;
